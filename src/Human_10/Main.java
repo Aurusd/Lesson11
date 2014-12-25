5bca52941eed49b60079877b6f5e480b6f51a0d5
Aurusd <rebrov@bigmir.net>
2014-12-25 14:16:33 +0200

    Update 1.0.1

--- src/Human_10/Main.java
+++ src/Human_10/Main.java
@@ -1,14 +1,15 @@
 package Human_10;
 
 
+import java.io.*;
 
-public class Main{
+public class Main implements Serializable {
 
 
     public static void main(String[] args) throws CloneNotSupportedException {
-        Human h1 = new Human("Dima",29, Human.Sex.MAN, Human.Temperament.MELANCHOLIC);
-        Human h2 = new Human("Dima",29, Human.Sex.MAN, Human.Temperament.PHLEGMATIC);
-        Human h3 = new Human("Vasil",30, Human.Sex.MAN,Human.Temperament.MELANCHOLIC);
+        Human h1 = new Human("Dima", 29, Human.Sex.MAN, Human.Temperament.MELANCHOLIC);
+        Human h2 = new Human("Dima", 29, Human.Sex.MAN, Human.Temperament.PHLEGMATIC);
+        Human h3 = new Human("Vasil", 30, Human.Sex.MAN, Human.Temperament.MELANCHOLIC);
 
         System.out.println("String 1-st is " + h1.toString());
         System.out.println("String 2-nd is " + h2.toString());
@@ -29,14 +30,37 @@ public class Main{
 
 
         Human h4 = (Human) h3.clone();
-        System.out.println("Human h4 is clone of Human h3 " + h4.Name + " " + h4.age  + " " + h4.sex);
+        System.out.println("Human h4 is clone of Human h3 " + h4.Name + " " + h4.age + " " + h4.sex);
         h3.Name = "Viktor";
-        h3.age =  39;
+        h3.age = 39;
 
         System.out.println(h3.Name + "," + h3.age);
         System.out.println(h4.Name + "," + h4.age);
 
-        }
+        try {
+            // запись
+            FileOutputStream fos = new FileOutputStream("temp.out");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            try {
+                oos.writeObject(h1);
+            } finally {
+                oos.close();
+            }
+
+            //чтение
+            Human h1_1;
+        FileInputStream fis = new FileInputStream("temp.out");
+        ObjectInputStream oin  = new ObjectInputStream(fis);
+            try {
+                h1_1 = (Human) oin.readObject();
+                System.out.println("Human Name" + h1_1.Name);
+            } finally {
+                oin.close();
+            }
+
+
+        } catch (IOException | ClassNotFoundException ex) {};
 
     }
+}
 
